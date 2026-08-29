3a71604eb4de66cf82a44381a27bb1bbdbb4688b
Derylfabiensyah <derylfabien09@gmail.com>
2026-08-29 21:13:53 +0700

    fix(mobile): disable kotlin incremental compilation and update gradle settings

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
